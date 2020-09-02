6f4ea59183d3d1e0ccf3e7173773c15a58bf7914
BonHowi <52290046+BonHowi@users.noreply.github.com>
2020-09-02 21:58:15 +0200

    Update classes.cpp

--- classes.cpp
+++ classes.cpp
@@ -1,6 +1,7 @@
 #include <sstream>
 #include <iostream>
 #include "classes.h"
+#include <vector>
 
 using namespace std;
 
@@ -8,182 +9,265 @@ using namespace std;
  * Rotor class
  */
 Rotor::Rotor() {
-	this->forward = NULL;
-	this->turns = NULL;
-	this->nTurns = 0;
-	this->size = 0;
-	this->index = 0;
+        this->forward = NULL;
+        this->turns = NULL;
+        this->turns = NULL;
+        this->nTurns = 0;
+        this->index = 0;
+
+        this->size = 0;
+        this->index = 0;
+        this->tableIncrements = NULL;
 }
 
-void Rotor::init(string in, string t, Alphabet& a) {
-	stringstream ss(in);
-	stringstream turns(t);
+void Rotor::rotateCables(Alphabet& a) {
+        for (int i = 0; i < this->size; i++) {
 
-	this->size = a.size;
-	turns >> this->nTurns;
+                this->cables[i] = (this->index + i);
+        }
 
-	this->forward = new int[size];
-	this->turns = new int[nTurns];
 
-	for (int i = 0; i < this->size; i++) {
-		string s;
-		ss >> s;
+}
 
-		int j = a.toIndex(s);
 
-		this->forward[i] = j;
-	}
+void Rotor::init(Alphabet& a) {
+        //stringstream ss(in);
+        //stringstream turns(t);
 
-	for (int i = 0; i < this->nTurns; i++) {
-		string s;
-		turns >> s;
-		int j = a.toIndex(s);
+        this->size = a.size;
+        //turns >> this->nTurns;
 
-		this->turns[i] = j;
-	}
+        this->forward = new int[size];
+        this->cables = new int[size];
+        this->tableIncrements = new int[size];
+        fill( tableIncrements, tableIncrements + sizeof( tableIncrements ), 0 );
+        //this->tableIncrements = new int[size];
+        //this->turns = new int[nTurns];
+        //cout << "table increments creation" << endl;
+        for (int i = 0; i < this->size; i++) {
+                string s;
+                //ss >> s;
+                cin >> s;
+                int j = a.toIndex(s);
 
-	this->index = 0;
+                this->forward[i] = j;
+                //cout << i << ": " << j << endl;
+                //vector<int> tableIncrements;
+                //int value = j - i;
+                //this->tableIncrements.push_back(value);
+                // cout << "table increments: " << i << "|" << j << " " << tableIncrements[i] << endl;
+
+                }
+
+
+        int nT;
+        scanf_s("%d", &nT);
+        //cin >> nT;
+        this->nTurns = nT;
+        this->turns = new int[this->nTurns];
+        for (int i = 0; i < this->nTurns; i++) {
+                string s;
+                //turns >> s;
+                cin >> s;
+                int j = a.toIndex(s);
+
+                this->turns[i] = j;
+        }
+
+        this->index = 0;
+        this->indexIncrement = this->size - 1;
+
+        for (int i = 0; i < this->size; i++) {
+                this->indexIncrement;
+                //cout << " increment " << i << " " << this->tableIncrements[i] << endl;
+        }
+        for (int i = 0; i < this->size; i++) {
+                this->tableIncrements[i] = (this->forwardSub(i) - i) % (this->size - 1);
+                //cout << this->forwardSub(i) << "  x  " << this->tableIncrements[i] << endl;
+        }
 }
 
 Rotor::~Rotor() {
-	delete[] this->forward;
-	delete[] this->turns;
+        delete[] this->forward;
+        delete[] this->turns;
 }
 
 void Rotor::setIndex(string s, Alphabet& a) {
-	int j = a.toIndex(s);
-	for (int i = 0; i < this->size; i++) {
-		if (this->forward[i] == j)
-			this->index = i;
-	}
+        int j = a.toIndex(s);
+        //cout <<"setting index " << j << endl;
+        for (int i = 0; i < this->size; i++) {
+                if (this->forward[i] == j)
+                        this->index = i;
+                cout << this->index << endl;
+        }
+}
+
+void Rotor::setIndex(int ind) {
+        this->index = (ind) % this->size;
+        //cout << "index: " << this->index << endl;
 }
 
 int Rotor::forwardSub(int i) {
-	return this->forward[(i + this->index) % this->size];
+        //cout << "forward sub" << this->forward[(i + this->index + this->tableIncrements[this->indexIncrement]) % this->size];
+        return this->forward[((this->size) + (i + this->index + this->tableIncrements[this->index])%this->size) % (this->size)];
+        //return this->forward[i];
 }
 
 
 int Rotor::reverseSub(int i) {
-	for (int j = 0; j < this->size; j++) {
-		if (this->forwardSub(j) == i)
-			return j;
-	}
-	return 0;
+        for (int j = 0; j < this->size; j++) {
+                if (this->forwardSub(j) == i)
+                        return j;
+        }
+        return 0;
 }
 
 bool Rotor::onTurnover()
 {
-	int j = this->forwardSub(0);
-	for (int i = 0; i < this->nTurns; i++) {
-		if (this->turns[i] == j)
-			return true;
-	}
-	return false;
+        int j = this->forwardSub(0);
+        for (int i = 0; i < this->nTurns; i++) {
+                if (this->turns[i] == j)
+                        return true;
+        }
+        return false;
 }
 
 void Rotor::increment() {
-	/*this->index--;
-	if (this->index < 0)
-		this->index = this->size - 1;*/
-	this->index++;
-	if (this->index > this->size - 1)
-		this->index = 0;
+        this->indexIncrement--;
+        if (this->indexIncrement < 0)
+        this->indexIncrement = this->size - 1;
+        this->index++;
+        if (this->index > this->size - 1)
+        this->index = 0;
+
+                //roznica z z id nastepnika i tego co wskazywal
+
+                //for (int i = 0; i < this->size; i++) {
+                //      this->tableIncrements[i] = (this->forwardSub(i) - i) % (this->size - 1);
+                //      cout << this->forwardSub(i) <<"  x  " << this->tableIncrements[i] << endl;
+                //}
+
+
+
+        //for (int i = 0; i < this->size; i++) {
+        //      cout << i << ": f|ti " << this->forward[i] << "  " << this->tableIncrements[i] << endl;
+        //}
+        ////for (auto i = this->tableIncrements.begin(); i != this->tableIncrements.end(); ++i)
+        ////    std::cout << *i << ' ' << endl;
+        //rotate(this->tableIncrements.begin(), this->tableIncrements.begin() + 1, this->tableIncrements.end());
+        //for (int i = 0; i < this->size; i++) {
+        //      cout << i << ": f|ti " << this->forward[i] << "  " << this->tableIncrements[i] << endl;
+        //}
+        //for (int i = 0; i < this->size; i++) {
+        //      this->forward[i] += (i + this->tableIncrements[i]) % this->size;
+        //      cout << i << ": f|ti " << this->forward[i] << "  " << this->tableIncrements[i] << endl;
+        //}
 }
 
 void Rotor::print(Alphabet& a) {
-	for (int i = 0; i < this->size; i++)
-		cout << a.toAlpha(i) << ": " << a.toAlpha(this->forwardSub(i)) << endl;
-	cout << endl;
+        cout << "Rotor:" << endl;
+        cout << "index: " << this->index << endl;
+        cout << "Rotor forward:" << endl;
+        for (int i = 0; i < this->size; i++)
+                cout << a.toAlpha(i) << ": " << a.toAlpha(this->forwardSub(i)) << endl;
+        cout << endl;
+        cout << "Rotor reverse:" << endl;
+        for (int i = 0; i < this->size; i++)
+                cout << a.toAlpha(i) << ": " << a.toAlpha(this->reverseSub(i)) << endl;
+        cout << endl;
 
-	for (int i = 0; i < this->size; i++)
-		cout << a.toAlpha(i) << ": " << a.toAlpha(this->reverseSub(i)) << endl;
-	cout << endl;
-
-	if (this->nTurns > 0) {
-		cout << "Turnover: ";
-		for (int i = 0; i < this->nTurns; i++)
-			cout << a.toAlpha(this->turns[i]) << " ";
-		cout << endl;
-	}
+        if (this->nTurns > 0) {
+                cout << "Turnover: ";
+                for (int i = 0; i < this->nTurns; i++)
+                        cout << a.toAlpha(this->turns[i]) << " ";
+                        cout << endl;
+        }
 
 }
-
 
 /**
  * Reflector class
  */
 Reflector::Reflector() {
-	this->reflections = NULL;
-	this->size = 0;
+        this->reflections = NULL;
+        this->size = 0;
 }
-void Reflector::init(string in, Alphabet& a) {
-	this->size = a.size;
-	this->reflections = new int[size];
 
-	stringstream ss(in);
+void Reflector::init(Alphabet& a) {
+        this->size = a.size;
+        this->reflections = new int[size];
 
-	for (int i = 0; i < this->size; i++) {
-		string s;
-		ss >> s;
-		int j = a.toIndex(s);
+        //stringstream ss(in);
 
-		this->reflections[i] = j;
-	}
+        for (int i = 0; i < this->size; i++) {
+                string s;
+                //ss >> s;
+                cin >> s;
+                int j = a.toIndex(s);
+
+                this->reflections[i] = j;
+        }
 }
 
 Reflector::~Reflector() {
-	delete[] this->reflections;
+        delete[] this->reflections;
 }
 
 int Reflector::reflect(int i) {
-	return this->reflections[i];
+        return this->reflections[i];
 }
 
 void Reflector::print(Alphabet& a) {
-	for (int i = 0; i < this->size; i++)
-		cout << a.toAlpha(i) << ": " << a.toAlpha(this->reflections[i]) << endl;
+        cout << "Reflector:" << endl;
+        for (int i = 0; i < this->size; i++)
+                cout << a.toAlpha(i) << ": " << a.toAlpha(this->reflections[i]) << endl;
 }
 
 /**
  * Alphabet class
  */
-
 Alphabet::Alphabet() {
-	this->size = 0;
-	this->letters = NULL;
+        this->size = 0;
+        this->letters = NULL;
 }
 
-void Alphabet::init(int size, string in) {
-	this->size = size;
-	this->letters = new string[size];
+Alphabet::Alphabet(int size) {
+        this->size = size;
+        this->letters = new string[size];
+}
 
-	stringstream ss(in);
+void Alphabet::init(/*int size,*/ string in, int iterator) {
+        //this->size = size;
+        //this->letters = new string[size];
 
-	for (int i = 0; i < size; i++)
-		ss >> this->letters[i];
+        //stringstream ss(in);
+
+        //for (int i = 0; i < size; i++)
+        /*ss >>*/ this->letters[iterator] = in;
 }
 
 Alphabet::~Alphabet() {
-	delete[] this->letters;
+        delete[] this->letters;
 }
 
 int Alphabet::toIndex(string s) {
-	for (int i = 0; i < this->size; i++) {
-		if (letters[i] == s)
-			return i;
-	}
-	return -1;
+        for (int i = 0; i < this->size; i++) {
+                if (letters[i] == s)
+                        return i;
+        }
+        return -1;
 }
 
 string Alphabet::toAlpha(int i) {
-	if (i < this->size)
-		return this->letters[i];
-	return "";
+        if (i < this->size)
+                return this->letters[i];
+        return "";
 }
 
 void Alphabet::print() {
-	for (int i = 0; i < this->size; i++)
-		cout << this->letters[i] << endl;
+        cout << "Alphabet:" << endl;
+        for (int i = 0; i < this->size; i++)
+                cout << this->letters[i] << endl;
 }
 
 /**
@@ -191,109 +275,117 @@ void Alphabet::print() {
  */
 
 Enigma::Enigma() {
-	this->rotors = NULL;
-	this->numRotors = 0;
+        this->rotors = NULL;
+        this->numRotors = 0;
 }
 
 Enigma::~Enigma() {
-	delete[] rotors;
+        delete[] rotors;
 }
 
 void Enigma::init(int numRotors, Alphabet& a) {
-	this->numRotors = numRotors;
-	this->rotors = new Rotor[numRotors];
-	this->alphabet = a;
+        this->numRotors = numRotors;
+        this->rotors = new Rotor[numRotors];
+        this->alphabet = a;
 }
 
 void Enigma::addRotor(Rotor& r, int i) {
-	this->rotors[i] = r;
+        this->rotors[i] = r;
 }
 
 void Enigma::setReflector(Reflector& r) {
-	this->reflector = r;
+        this->reflector = r;
 }
 
 void Enigma::print() {
-	cout << "--Alphabet--" << endl;
-	this->alphabet.print();
-	cout << endl << endl;
+        cout << "--Alphabet--" << endl;
+        this->alphabet.print();
+        cout << endl << endl;
 
-	cout << "--Rotors--" << endl;
-	for (int i = 0; i < this->numRotors; i++) {
-		this->rotors[i].print(this->alphabet);
-		cout << endl;
-	}
-	cout << endl;
+        cout << "--Rotors--" << endl;
+        for (int i = 0; i < this->numRotors; i++) {
+                this->rotors[i].print(this->alphabet);
+                cout << endl;
+        }
+        cout << endl;
 
 
-	cout << "--Reflector--" << endl;
-	this->reflector.print(this->alphabet);
-	cout << endl;
+        cout << "--Reflector--" << endl;
+        this->reflector.print(this->alphabet);
+        cout << endl;
 }
 
 string Enigma::encryptLetter(int letter)
 {
-	// increment first rotor before encoding
-	this->rotors[0].increment();
-	//this->rotors[0].print(alphabet);
+        // increment first rotor before encoding
+        //if(letter != 0) this->rotors[0].increment();
+        //this->rotors[0].print(alphabet);
 
-	//cout << alphabet.toAlpha(letter) << " -> ";
-	// pass through rotors
-	for (int i = 0; i < this->numRotors; i++) {
-		// if on turnover letter
-		if (this->rotors[i].onTurnover()) {
-			// if not last rotor
-			if (i < this->numRotors) {
-				this->rotors[i + 1].increment();		// increment next rotor
-			}
-		}
+        //cout << alphabet.toAlpha(letter) << " -> ";
+        // pass through rotors
+        for (int i = 0; i < this->numRotors; i++) {
+                // if on turnover letter
+                if (this->rotors[i].onTurnover()) {
+                        // if not last rotor
+                        if (i < this->numRotors && i < 3) {
+                                this->rotors[i + 1].increment();                // increment next rotor
+                                if (this->numRotors > 1) {
+                                        this->rotors[i].increment();
+                                }
+                        }
+                }
 
-		// pass through rotor
-		letter = this->rotors[i].forwardSub(letter);
+                // pass through rotor
+                letter = this->rotors[i].forwardSub(letter);
 
-		//cout << "[Rotor " << i + 1 << "] -> " << alphabet.toAlpha(letter) << " -> ";
-	}
+                //cout << "[Rotor " << i + 1 << "] -> " << alphabet.toAlpha(letter) << " -> ";
+        }
 
-	// pass through reflector
-	letter = this->reflector.reflect(letter);
-	//cout << "[Reflector] -> " << alphabet.toAlpha(letter) << " -> ";
+        // pass through reflector
+        letter = this->reflector.reflect(letter);
+        cout << "[Reflector] -> " << alphabet.toAlpha(letter) << " -> ";
+//
 
+        // pass through rotors in reverse
+        for (int i = this->numRotors - 1; i >= 0; i--) {
+                letter = this->rotors[i].reverseSub(letter);
+                //cout << "[Rotor " << i + 1 << "] -> " << alphabet.toAlpha(letter) << " -> ";
+        }
 
-	// pass through rotors in reverse
-	for (int i = this->numRotors - 1; i >= 0; i--) {
-		letter = this->rotors[i].reverseSub(letter);
-		//cout << "[Rotor " << i + 1 << "] -> " << alphabet.toAlpha(letter) << " -> ";
-	}
-
-	//cout << "RETURN" << endl;
-	// return encrypted letter
-	return this->alphabet.toAlpha(letter);
+        //cout << "RETURN" << endl;
+        // return encrypted letter
+        return this->alphabet.toAlpha(letter);
 }
 
 void Enigma::setString(string enc) {
-	this->enc = enc;
+        this->enc = enc;
 }
 
+
 string Enigma::encrypt() {
-	string s;
+        string s;
 
-	// until the string ends
-	while (this->enc[0] != '$') {
-		// match letter
-		for (int i = 0; i < this->alphabet.size; i++) {
-			// on match
-			if (this->enc.find(this->alphabet.letters[i]) == 0) {
-				// append encrypted letter to s
-				string t = this->encryptLetter(i);
-				s += t;
+        // until the string ends
+        int encryptedCount = 0;
+        while (this->enc[0] != '0') {
+                // match letter
+                for (int i = 0; i < this->alphabet.size; i++) {
+                        // on match
+                        if (this->enc.find(this->alphabet.letters[i]) == 0) {
+                                // append encrypted letter to s
+                                if (encryptedCount != 0) this->rotors[0].increment();
+                                string t = this->encryptLetter(i);
+                                s += t;
+                                s += " ";
 
-				//cout << this->alphabet.letters[i] << " -> " << t << endl;
+                                cout << this->alphabet.letters[i] << " -> " << t << endl;
 
-				// remove letter from enc
-				this->enc.erase(0, this->alphabet.letters[i].length());
-			}
-		}
-	}
+                                // remove letter from enc
+                                this->enc.erase(0, this->alphabet.letters[i].length());
+                                encryptedCount++;
+                        }
+                }
+        }
 
-	return s;
+        return s;
 }
